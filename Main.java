9493049fa6904635d1062cea8987bb170aa8b63f
mba <aipaixz@gmail.com>
2025-04-21 17:32:49 +0800

    create tupleclients

--- Main.java
+++ Main.java
@@ -7,10 +7,16 @@ public class Main {
         List<Thread> threads = new ArrayList<>();
         TupleClient [] clients = new TupleClient[10];
         for (int i = 0; i < clients.length; i++) {
-            final int index = i; 
-            clients[i] = new TupleClient("test-workload/client_" + (i+1) + ".txt");
-            Thread thread = new Thread(() -> clients[i].startRead());
+            final int index = i+1; 
+            clients[i] = new TupleClient("test-workload/client_" + (index) + ".txt");
+            Thread thread = new Thread(() -> clients[index].startRead());
             threads.add(thread);
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
         }
 
     }

56765c16600b889f434418e6e6de2dbfb67d6efc
mba <aipaixz@gmail.com>
2025-04-21 16:31:22 +0800

    set model to read put and get

--- Main.java
+++ Main.java
@@ -6,7 +6,10 @@ public class Main {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.println(line); 
+                String [] parts = line.split(" ");
+                if (parts[0]=="READ"){
+                    
+                }
             }
         } catch (IOException e) {
             System.err.println("Error file: " + e.getMessage());
